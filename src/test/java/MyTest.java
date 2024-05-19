98904009d157f868f14e85ed9d7aa881cf00f0cc
Alex330790 <ajkul@mail.ru>
2024-05-19 23:20:08 +0300

    Add new tests

--- src/test/java/MyTest.java
+++ src/test/java/MyTest.java
@@ -2,7 +2,8 @@ import org.junit.jupiter.api.Assertions;
 
 public class MyTest {
     @org.junit.jupiter.api.Test
-    public void testConcat_validArgument_success() {
+    public void testValidArguments() {
+        //совпадение результата с ожидаемым - true
         //Maven
         // given:
         final int[][] teamsOriginal = {
@@ -19,4 +20,41 @@ public class MyTest {
         // then:
         Assertions.assertArrayEquals(teamsExpected, teamResult);
     }
+    @org.junit.jupiter.api.Test
+    public void testNealidArguments() {
+        //не должен обрабатывать массивы с меньшим кол-вом элементов
+        //Maven
+        // given:
+        final int[][] teamsOriginal = {
+                {45, 31, 24, 22, 20, 17, 14, 13, 12, 10},
+                {31, 18, 15, 12, 10, 8, 6, 4, 2, 1},
+                {400, 30, 10, 9, 8, 7, 6, 5, 2}
+        };
+        // when:
+        try {
+            final int[] teamResult = Main.mergeAll(teamsOriginal);
+//            Assertions.assertTrue(false);
+        }catch (NullPointerException e){
+            Assertions.assertTrue(e.getClass().equals("NullPointerException"));
+        }
+    }
+    @org.junit.jupiter.api.Test
+    public void testLength() {
+        //Длина выведенного массива - 10
+        final int[][] teamsOriginal = {
+                {45, 31, 24, 22, 20, 17, 14, 13, 12, 10},
+                {31, 18, 15, 12, 10, 8, 6, 4, 2, 1},
+                {51, 30, 10, 9, 8, 7, 6, 5, 2, 1}
+        };
+        final int lentghDim = 10;
+
+
+        // when:
+        final int[] teamResult = Main.mergeAll(teamsOriginal);
+
+        // then:
+        Assertions.assertTrue(teamResult.length==lentghDim);
+
+    }
+
 }
